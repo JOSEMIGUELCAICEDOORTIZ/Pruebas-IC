666c55cbbb91f4fca9ed59a0d78d0e064b745fb0
JOSEMIGUELCAICEDOORTIZ <jose.miguel.caicedo@correounivalle.edu.co>
2023-06-24 09:30:31 -0500

    Hola mundo

---
+++ test_main.py
@@ -0,0 +1,11 @@
+from main import sum,max
+
+
+def test_sum():
+    assert sum(3,8)==11
+    print("La función se ejecuta correctamente")
+def test_max():
+    assert max(11,10)==true
+    
+    if "__name__" == "__main__" :
+        test_sum
